912010f63d73736d1279702d60dc943b9c04aa86
Bobae Kang <bkang.kr@gmail.com>
2019-05-07 14:33:13 -0500

    switch to btn

--- src/components/BasePropChip.vue
+++ src/components/BasePropChip.vue
@@ -1,5 +1,32 @@
 <template>
-  <v-chip disabled small text-color="#333" class="font-lato uppercase">
+  <!-- <v-chip disabled small text-color="#333" class="font-lato uppercase">
     <slot></slot>
-  </v-chip>
+  </v-chip> -->
+  <v-btn
+    class="chip"
+    small
+    depressed
+    round
+    @click="$emit('chip-click', $event.target.textContent)"
+  >
+    <slot></slot>
+  </v-btn>
 </template>
+
+<script>
+export default {
+  props: {
+    event: Function
+  }
+};
+</script>
+
+<style scoped>
+.chip {
+  background-color: #e0e0e0 !important;
+  color: #333;
+  margin: 4px;
+  min-width: 0;
+  text-transform: uppercase;
+}
+</style>
