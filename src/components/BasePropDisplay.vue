3e4cf10da3fcfc2c22d597b5ef924d31c6b7eef2
Bobae Kang <bkang.kr@gmail.com>
2019-05-16 14:50:26 -0500

    make functional

--- src/components/BasePropDisplay.vue
+++ src/components/BasePropDisplay.vue
@@ -1,6 +1,6 @@
-<template>
+<template functional>
   <div class="py-1 font-lato small">
-    <span v-if="name" class="bold pr-2">{{ name }}</span>
+    <span v-if="props.name" class="bold pr-2">{{ props.name }}</span>
     <slot></slot>
   </div>
 </template>
